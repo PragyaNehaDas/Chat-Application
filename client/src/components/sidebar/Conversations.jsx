97d5560babdf4ca4ed43223d80e4221f6b15ea24
PragyaNeha Das <142140081+PragyaNehaDas@users.noreply.github.com>
2024-08-22 13:59:48 +0530

    Update Conversations.jsx

--- client/src/components/sidebar/Conversations.jsx
+++ client/src/components/sidebar/Conversations.jsx
@@ -1,28 +1,3 @@
-// import React from "react";
-// import Conversation from "./Conversation";
-// import useGetConversation from "../../hooks/useGetConversation";
-
-// const Conversations = () => {
-//   const { loading, conversations } = useGetConversation();
-
-//   return (
-//     <div className="py-2 flex flex-col max-h-[350px] overflow-y-auto ">
-//       {conversations.map((conversation, index) => {
-//         <Conversation
-//           key={conversation._id}
-//           conversation={conversation}
-//           lastIndex={index === conversation.length - 1}
-//         />;
-//       })}
-//       {/* <Conversation />
-//        */}
-//     </div>
-//   );
-// };
-
-// export default Conversations;
-
-
 import React from "react";
 import Conversation from "./Conversation";
 import useGetConversation from "../../hooks/useGetConversation";
